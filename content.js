75ab19737ecd1117ec9fc168c15c1712a599be72
Robert Keresnyei <robert.keresnyei@purepoint.io>
2017-12-02 19:17:59 +0100

    Handle JIRA prefix dynamically

--- content.js
+++ content.js
@@ -3,15 +3,27 @@ chrome.runtime.onMessage.addListener(function(message) {
   if (branchElements.length !== 2) {
     return;
   }
-  const branch = document.getElementsByClassName('branch')[1].getElementsByClassName('css-truncate-target')[0].innerHTML.substring(0, 8);
-  const cardUrl = `https://jira.bgchtest.info/browse/${branch}`;
+  const branch = document
+                   .getElementsByClassName('branch')[1]
+                   .getElementsByClassName('css-truncate-target')[0]
+                   .innerHTML;
+
+  const cardIdRegexPattern = '[A-Z0-9]+-[0-9]+';
+  const cardId = new RegExp(cardIdRegexPattern).exec(branch)[0];
+  const cardUrl = `https://jira.bgchtest.info/browse/${cardId}`;
   const xhr = new XMLHttpRequest();
   xhr.open('GET', cardUrl, true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
       const parser = new DOMParser();
       const response = parser.parseFromString(xhr.responseText, 'text/html');
-      const title = response.getElementsByTagName('title')[0].innerHTML.replace(/^\[(.{8})\]/, '$1').slice(0, -12);
+      const replaceRegex = new RegExp(`^\\[(${cardIdRegexPattern})\\]`);
+      const title = /(.*) - .*$/.exec(response
+                                        .getElementsByTagName('title')[0]
+                                        .innerHTML
+                                        .replace(replaceRegex, '$1')
+                                      )[1];
+
       if (message.mode === 'all') {
         document.getElementById('pull_request_title').value = title;
       }
